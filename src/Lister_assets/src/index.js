de8b5c514f289912d4d231ce6f7be54940cadfd3
Shogun <1790333@edu.vaniercollege.qc.ca>
2022-05-15 20:29:36 -0400

    react added

--- src/Lister_assets/src/index.js
+++ src/Lister_assets/src/index.js
@@ -1,19 +1,5 @@
-import { Lister } from "../../declarations/Lister";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await Lister.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
-});
+ReactDOM.render(<App />, document.getElementById("root"));
